24d937cff4c6769440ea67eed0d759b51d0d4784
Xanonymous <trusaidlin@gmail.com>
2024-01-31 16:35:47 +0000

    feat: 🎸 add bootstrap mount point for both style and script

---
+++ src/initBootstrap.ts
@@ -0,0 +1,18 @@
+// Import bootstrap's scss to make sure it is bundled.
+import './css/main.scss'
+
+// Import bootstrap's javascript to make sure it is bundled.
+// This can be optimized by importing only the components that are used.
+import 'bootstrap/js/dist/base-component'
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/offcanvas'
+import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/toast'
+import 'bootstrap/js/dist/tooltip'
