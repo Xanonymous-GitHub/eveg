a2f255992fadda2e50dfbb443c20341238d51144
Xanonymous <trusaidlin@gmail.com>
2024-02-06 14:43:44 +0000

    refactor: 💡 update typescript config for using static env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
+import 'vite/client'
+
 /// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+  readonly ENABLE_BOOTSTRAP_REBOOT?: string
+}
