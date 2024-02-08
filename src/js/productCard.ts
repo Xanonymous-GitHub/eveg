f8aabde3cc651eeb394e2adc8554772b1a49727f
Xanonymous <trusaidlin@gmail.com>
2024-02-08 14:15:20 +0000

    style: 💄 reformat html files

--- src/js/productCard.ts
+++ src/js/productCard.ts
@@ -2,33 +2,33 @@ import type { Product } from './typing.ts'
 
 const cardTemplate: string = `
   <div class="shop-product card p-2 w-100" data-num="{{ ID }}">
-    <div class="shop-product-details shop-product-title card__title text-center" data-field="title" data-num="{{ ID }}">
-      <h3>{{ TITLE }}</h3>
+  <div class="shop-product-details shop-product-title card__title text-center" data-field="title" data-num="{{ ID }}">
+    <h3>{{ TITLE }}</h3>
+  </div>
+  <div class="card__content d-flex flex-column justify-content-center" data-num="{{ ID }}">
+    <div class="shop-product-details shop-product-img m-auto" data-field="img" data-num="{{ ID }}">
+      <img src="{{ IMG_SRC }}" class="img-fluid" alt="" loading="lazy">
     </div>
-    <div class="card__content d-flex flex-column justify-content-center" data-num="{{ ID }}">
-      <div class="shop-product-details shop-product-img m-auto" data-field="img" data-num="{{ ID }}">
-        <img src="{{ IMG_SRC }}" class="img-fluid" alt="" loading="lazy">
-      </div>
-      <div class="shop-product-details shop-product-price m-auto" data-field="price" data-num="{{ ID }}">
-        <span>{{ PRICE }}</span>
-      </div>
-      <div class="shop-product-details shop-product-units m-auto" data-field="units" data-num="{{ ID }}">
-        <span>{{ UNITS }}</span>
-      </div>
-      <div class="shop-product-buying m-auto" data-num="{{ ID }}">
-        <div class="productBasketDiv m-auto">
-          <button class="addToBasket m-auto d-block btn btn-warning">Add to Basket</button>
-          <div class="adjustDiv my-2">
-            <span class="m-auto d-flex justify-content-center input-group">
-              <button class="btn adjustDown">-</button>
-              <input class="buyInput form-control" data-num="{{ ID }}" min="0" type="number" value="0">
-              <button class="btn adjustUp">+</button>
-            </span>
-          </div>
+    <div class="shop-product-details shop-product-price m-auto" data-field="price" data-num="{{ ID }}">
+      <span>{{ PRICE }}</span>
+    </div>
+    <div class="shop-product-details shop-product-units m-auto" data-field="units" data-num="{{ ID }}">
+      <span>{{ UNITS }}</span>
+    </div>
+    <div class="shop-product-buying m-auto" data-num="{{ ID }}">
+      <div class="productBasketDiv m-auto">
+        <button class="addToBasket m-auto d-block btn btn-warning">Add to Basket</button>
+        <div class="adjustDiv my-2">
+          <span class="m-auto d-flex justify-content-center input-group">
+          <button class="btn adjustDown">-</button>
+          <input class="buyInput form-control" data-num="{{ ID }}" min="0" type="number" value="0">
+          <button class="btn adjustUp">+</button>
+          </span>
         </div>
       </div>
     </div>
   </div>
+</div>
 `
 
 // TODO: Migrate to TSX or WebComponents.
