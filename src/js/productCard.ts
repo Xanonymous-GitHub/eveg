47029d3683bd2350c6cdeea09334e9a9f4dac6ac
Regan <regan.kastelie@gmail.com>
2024-02-14 15:40:47 +0000

    convert js to ts

--- src/js/productCard.ts
+++ src/js/productCard.ts
@@ -2,26 +2,27 @@ import type { Product } from './typing.ts'
 
 const cardTemplateStr: string = `
   <div class="shop-product card p-2 w-100" data-num="{{ ID }}">
+  <div class="shop-product-details shop-product-img m-auto" data-field="img" data-num="{{ ID }}">
+    </div>
   <div class="shop-product-details shop-product-title card__title text-center" data-field="title" data-num="{{ ID }}">
-    <h3 class="text-nowrap overflow-hidden">{{ TITLE }}</h3>
+    <h2 class="text-nowrap overflow-hidden">{{ TITLE }}</h2>
   </div>
   <div class="card__content d-flex flex-column justify-content-center" data-num="{{ ID }}">
-    <div class="shop-product-details shop-product-img m-auto" data-field="img" data-num="{{ ID }}">
-    </div>
-    <div class="shop-product-details shop-product-price m-auto" data-field="price" data-num="{{ ID }}">
+    
+    <span class="shop-product-details shop-product-price m-auto" data-field="price" data-num="{{ ID }}">
       <span>{{ PRICE }}</span>
-    </div>
-    <div class="shop-product-details shop-product-units m-auto" data-field="units" data-num="{{ ID }}">
+    </span>
+    <span class="shop-product-details shop-product-units m-auto" data-field="units" data-num="{{ ID }}">
       <span>{{ UNITS }}</span>
-    </div>
+    </span>
     <div class="shop-product-buying m-auto" data-num="{{ ID }}">
       <div class="productBasketDiv m-auto">
         <button class="addToBasket m-auto d-block btn btn-warning">Add to Basket</button>
-        <div class="adjustDiv my-2">
+        <div class="adjustDiv my-2 d-none">
           <span class="m-auto d-flex justify-content-center input-group">
-          <button class="btn adjustDown">-</button>
-          <input class="buyInput form-control" data-num="{{ ID }}" min="0" type="number" value="0">
-          <button class="btn adjustUp">+</button>
+            <button class="btn adjustDown">-</button>
+            <input class="buyInput form-control" data-num="{{ ID }}" min="0" type="number" value="1">
+            <button class="btn adjustUp">+</button>
           </span>
         </div>
       </div>
@@ -43,6 +44,7 @@ function createProductImageElement(src: string): HTMLImageElement {
 export function createProductCard(
   product: Product,
   onAddToBasketRequested: (productId: number, requestedQuantity: number) => void,
+  onSetProductQuantity: (productId: number, requestedQuantity: number) => void
 ): HTMLDivElement {
   const cardHTMLStr = cardTemplateStr
     .replaceAll('{{ ID }}', product.id.toString())
@@ -56,18 +58,68 @@ export function createProductCard(
 
   const inputBox = thisProductCard.querySelector('.buyInput') as HTMLInputElement
 
-  thisProductCard.querySelector('.adjustUp')?.addEventListener('click', () => {
+  inputBox?.addEventListener('change',
+  () => {
+    if (inputBox.value === "0" || inputBox.value === ""){
+      const addToBasketBtn = thisProductCard.querySelector('.addToBasket');
+      const adjustDiv = addToBasketBtn!.closest('.shop-product')!.querySelector('.adjustDiv');
+      addToBasketBtn!.classList.remove('d-none');
+      adjustDiv!.classList.add('d-none');
+    }
+    else{
+      onSetProductQuantity(product.id, Number.parseInt(inputBox.value))
+    }
+  })
+
+  inputBox?.addEventListener('keyup',
+  (e) => {
+    if (e.key === 'Enter' || e.keyCode === 13){
+      if (inputBox.value === "0" || inputBox.value ===""){
+        const addToBasketBtn = thisProductCard.querySelector('.addToBasket');
+        const adjustDiv = addToBasketBtn!.closest('.shop-product')!.querySelector('.adjustDiv');
+        addToBasketBtn!.classList.remove('d-none');
+        adjustDiv!.classList.add('d-none');
+      }
+      else{
+        onSetProductQuantity(product.id, Number.parseInt(inputBox.value))
+      }
+    }
+  })
+  
+  thisProductCard.querySelector('.adjustUp')?.addEventListener('click',
+   () => {
+    onAddToBasketRequested(product.id, 1)
     inputBox.value = (Number.parseInt(inputBox.value) + 1).toString()
   })
 
-  thisProductCard.querySelector('.adjustDown')?.addEventListener('click', () => {
+  
+
+  thisProductCard.querySelector('.adjustDown')?.addEventListener('click', (e) => {
+    onAddToBasketRequested(product.id, -1)
     const newValue = Number.parseInt(inputBox.value) - 1
-    inputBox.value = newValue < 0 ? '0' : newValue.toString()
+    inputBox.value = newValue <= 0 ? '1' : newValue.toString()
+    if (newValue === 0){
+      const addToBasketBtn = thisProductCard.querySelector('.addToBasket');
+      const adjustDiv = addToBasketBtn!.closest('.shop-product')!.querySelector('.adjustDiv');
+      addToBasketBtn!.classList.remove('d-none');
+      adjustDiv!.classList.add('d-none');
+    }
   })
 
   thisProductCard.querySelector('.addToBasket')?.addEventListener(
     'click',
-    () => onAddToBasketRequested(product.id, Number.parseInt(inputBox.value)),
+    function(e) { 
+      
+      onAddToBasketRequested(product.id, 1)
+      const addToBasketBtn = e.target as HTMLDivElement;
+      const adjustDiv = addToBasketBtn!.closest('.shop-product')!.querySelector('.adjustDiv');
+      // window.alert(adjustDiv.classList);
+      // window.alert(addToBasketBtn.classList)
+      addToBasketBtn.classList.add('d-none');
+      adjustDiv!.classList.remove('d-none');
+      const newValue = Number.parseInt(inputBox.value)
+      inputBox.value = newValue <= 0 ? '1' : newValue.toString()
+    },
   )
 
   const img = createProductImageElement(`/images/${product.imgName}`)
